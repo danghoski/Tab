674e687966ea72938bc9c4aa01e0682e0079a6af
danghoski <danghoskinfotech@gmail.com>
2021-06-30 17:56:52 +0100

    Initial commit

---
+++ app/src/main/java/com/aghogho/tab/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.aghogho.tab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aghogho.tab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var whatsAppAdapter: WhatsAppAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        whatsAppAdapter = WhatsAppAdapter(supportFragmentManager)
+        binding.pager.adapter = whatsAppAdapter
+        binding.tabLayout.setupWithViewPager(binding.pager)
+    }
+}
